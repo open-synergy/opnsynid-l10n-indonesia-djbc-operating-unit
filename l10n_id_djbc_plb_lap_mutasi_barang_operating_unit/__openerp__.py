c0af690f99edccbf9743470e842798406d204fd5
Andhitia Rama <andhitia.r@gmail.com>
2020-09-24 10:54:38 +0700

    [l10n_id_djbc_plb_lap_mutasi_barang_operating_unit] 8.0.1.0.0

---
+++ l10n_id_djbc_plb_lap_mutasi_barang_operating_unit/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Lap. Mutasi Barang for DJBC's "
+            "Pusat Logistik Berikat Operating Unit",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "l10n_id_djbc_plb_lap_mutasi_barang",
+        "product_operating_unit",
+        "stock_operating_unit",
+    ],
+    "data": [
+        "security/plb_lap_mutasi_barang_security.xml",
+    ],
+}
